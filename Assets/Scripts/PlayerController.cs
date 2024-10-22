2e77a5da3fb37722eaeae614a9e719a8d203cafe
aqarix <108086206+aqarix@users.noreply.github.com>
2024-10-22 22:42:07 +0200

    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jump;
+    bool doubleJump;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded())
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jump);
+                doubleJump = true;
+            }
+            else if(doubleJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jump);
+                doubleJump = false;
+            }
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+}
